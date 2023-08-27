330fc439418902e630cc783104e5ba492decc041
Zemmouri Farouk <faroukwebdesign@gmail.com>
2023-08-27 02:46:54 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+const domContent = document.addEventListener('DOMContentLoaded', function(){
+    const textReplacement = document.getElementById('text');
+    textReplacement.textContent = "This is really cool!"
+});
